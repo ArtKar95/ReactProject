cf5931174274c377108a3d44d05b22bbd58970c0
ArtKar95 <artkarapetyan1995@gmail.com>
2020-09-29 20:09:41 +0400

    correct-version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-// import Person from "./Components/Person";
-import Product from "./Homework4/Product";
 
 function App() {
   return (
