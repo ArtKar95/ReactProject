d4593a8bc26c1326de78d76c5e0b0b5c34222025
ArtKar95 <70438665+ArtKar95@users.noreply.github.com>
2020-12-31 01:25:03 +0400

    Lesson 29 (#11)
    * making design changes
    
    * delete debag.log
    
    * creating pages
    
    * creating page
    
    * my version Redux
    
    * redux advanced
    
    * Redux
    
    * using .env taskPage changing
    
    * search, change env
    
    * search, change env

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import toDoReduser from "./reduser";
@@ -10,12 +11,6 @@ if (process.env.NODE_ENV === "development") {
 
 const middleWares = applyMiddleware(...middlewaresArr);
 
-const store = createStore(
-  toDoReduser,
-  compose(
-    middleWares,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(toDoReduser, composeWithDevTools(middleWares));
 
 export default store;
